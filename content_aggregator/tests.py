38240f34beba169b44dd2f0623bc6837f5f82576
endlesstrax <ricky@whitelionmedia.com>
2021-01-11 21:25:16 -0500

    Added basic test

--- content_aggregator/tests.py
+++ content_aggregator/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.utils import timezone
+
+from .models import Entry
+
+
+@pytest.fixture
+def test_entry(db):
+    entry = Entry.objects.create(
+        title = "Awesome Article Title",
+        description = "A great article about all the things.",
+        pub_date = timezone.now(),
+        link = "http://myawesomeblog.com",
+        category = "ARTICLE"
+    )
+    return entry
+
+def test_class_str_repr(test_entry):
+    assert str(test_entry) == "Awesome Article Title"
