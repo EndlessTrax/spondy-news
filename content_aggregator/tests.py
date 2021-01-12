bd26c8fa0a4bb53892d978cd927d8f3b3ef9573e
endlesstrax <ricky@whitelionmedia.com>
2021-01-11 23:08:49 -0500

    Added further tests

--- content_aggregator/tests.py
+++ content_aggregator/tests.py
@@ -1,6 +1,8 @@
-import pytest
-
+from django.urls import reverse
 from django.utils import timezone
+
+import pytest
+from pytest_django.asserts import assertTemplateUsed
 
 from .models import Entry
 
@@ -19,3 +21,20 @@ def test_entry(db):
 
 def test_class_str_repr(test_entry):
     assert str(test_entry) == "Awesome Article Title"
+
+
+def test_entry_content(test_entry):
+    assert test_entry.link == "http://myawesomeblog.com"
+    assert test_entry.category == "ARTICLE"
+
+
+@pytest.mark.django_db
+def test_homepage_status_code(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_homepage_uses_correct_template(client):
+    response = client.get(reverse("homepage"))
+    assertTemplateUsed(response, "homepage.html")
