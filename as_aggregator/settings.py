89147593b049a509ffa69d7635d4730cce1bc3ac
endlesstrax <ricky@whitelionmedia.com>
2021-01-12 14:45:18 -0500

    Added apscheduler and logging

--- as_aggregator/settings.py
+++ as_aggregator/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third Party
-    #
+    "django_apscheduler",
     # My Apps
     "content_aggregator.apps.ContentAggregatorConfig",
 ]
@@ -125,3 +125,19 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
