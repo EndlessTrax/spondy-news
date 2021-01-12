3f4300ce1bb9001a994c9051136be3f3c3a9951f
endlesstrax <ricky@whitelionmedia.com>
2021-01-12 14:46:02 -0500

    Removed limit of 10

--- content_aggregator/views.py
+++ content_aggregator/views.py
@@ -9,5 +9,5 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["entries"] = Entry.objects.filter().order_by("-pub_date")[:10]
+        context["entries"] = Entry.objects.filter().order_by("-pub_date")
         return context
