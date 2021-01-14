e056cf20cd0765dfbef4c484e129860992cbedee
endlesstrax <ricky@whitelionmedia.com>
2021-01-14 12:51:03 -0500

    Seperated categories into own context fields.

--- content_aggregator/views.py
+++ content_aggregator/views.py
@@ -9,5 +9,17 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["entries"] = Entry.objects.filter().order_by("-pub_date")
+        
+        context["articles"] = Entry.objects.filter(
+            is_published=True, category="ARTICLE"
+        ).order_by("-pub_date")
+
+        context["research"] = Entry.objects.filter(
+            is_published=True, category="RESEARCH"
+        ).order_by("-pub_date")
+        
+        context["events"] = Entry.objects.filter(
+            is_published=True, category="EVENTS"
+        ).order_by("-pub_date")
+        
         return context
