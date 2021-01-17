ccf88af985753a2bac298709c0e99b8b33b5046c
endlesstrax <ricky@whitelionmedia.com>
2021-01-17 15:29:44 -0500

    Added custom action to admin panel

--- content_aggregator/admin.py
+++ content_aggregator/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Entry
 
+def publish_selected(modeladmin, request, queryset):
+    queryset.update(is_published=True)
+
+publish_selected.short_description = "Publish the selected posts"
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ("pub_date", "title", "category", "is_featured", "is_published")
+    actions= [publish_selected]
