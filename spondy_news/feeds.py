2781b44191cb51de83a55ad26cbb4d8b9258fe26
endlesstrax <ricky@whitelionmedia.com>
2021-01-23 09:49:38 -0500

    Increases items in RSS feed to 50

--- spondy_news/feeds.py
+++ spondy_news/feeds.py
@@ -3,13 +3,14 @@ from aggregator.models import Entry
 
 import uuid
 
+
 class LatestEntriesFeed(Feed):
     title = "Spondy News Feed"
     link = "https://spondy.news"
     description = "Published entries on Spondy News"
 
     def items(self):
-        return Entry.objects.filter(is_published=True).order_by("-pub_date")[:25]
+        return Entry.objects.filter(is_published=True).order_by("-pub_date")[:50]
 
     def item_title(self, item):
         return item.title
