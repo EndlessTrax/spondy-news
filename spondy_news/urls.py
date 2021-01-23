f52e9666424b6d78b2f263552705ce204559252a
endlesstrax <ricky@whitelionmedia.com>
2021-01-23 09:50:05 -0500

    named the RSS feed path

--- spondy_news/urls.py
+++ spondy_news/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
-    path("feeds/latest/", LatestEntriesFeed())
+    path("feeds/latest/rss.xml", LatestEntriesFeed(), name="rss_feed_latest")
 ]
