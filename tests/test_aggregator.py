8a82e53ebad8a75d8547fe9200652fd24af176c3
endlesstrax <ricky@whitelionmedia.com>
2021-02-17 15:18:43 -0500

    Added test for the job for deleting old content

--- tests/test_aggregator.py
+++ tests/test_aggregator.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 import pytest
 from pytest_django.asserts import assertTemplateUsed
 
+from aggregator.management.commands.startjobs import delete_rejected_entries
+from aggregator.models import Entry
 
 def test_class_str_repr(example_entry):
     """Tests the models __str__ function"""
@@ -29,6 +31,18 @@ def test_homepage_uses_correct_template(client):
     assertTemplateUsed(response, "homepage.html")
 
 
+@pytest.mark.django_db
+def test_delete_rejected_entries_job(example_entry, example_old_entry):
+    """Tests that the delete_rejected_entries function only deletes entries
+    that have is_published=False and a pub_date older than 14 days."""
+    assert Entry.objects.filter(title=example_entry.title).exists()
+    assert Entry.objects.filter(title=example_old_entry.title).exists()
+    
+    # Delete only unpublished entries over 14 days old. 
+    delete_rejected_entries()
+
+    assert Entry.objects.filter(title=example_entry.title).exists()
+    assert not Entry.objects.filter(title=example_old_entry.title).exists()
 
 
 
