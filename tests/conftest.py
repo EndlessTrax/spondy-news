54c4a967e768ca86da7b9c98981c137d185401e2
endlesstrax <ricky@whitelionmedia.com>
2021-02-17 15:18:15 -0500

    Added fixture for an old article for testing the deletion of old content

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pytest
 from django.utils import timezone
 
@@ -12,6 +14,19 @@ def example_entry(db):
         description="A great article about all the things.",
         pub_date=timezone.now(),
         link="http://myawesomeblog.com",
+        category="ARTICLE",
+    )
+    return entry
+
+
+@pytest.fixture
+def example_old_entry(db):
+    """Fixture for an old article"""
+    entry = Entry.objects.create(
+        title="Crap Article Title",
+        description="A terrible article about irrelevant things.",
+        pub_date=timezone.now() - timedelta(days=21),
+        link="http://mydodgyblog.com/",
         category="ARTICLE",
     )
     return entry
